82a871861a672252267c46ddcba1c27ff140d01f
Kenan-Kamel <kenanalghythee@gmail.com>
2021-11-19 22:37:16 -0600

    Adding Reaction UI

---
+++ SportCaveApp/app/src/main/java/com/example/sportcaveapp/Reaction.java
@@ -0,0 +1,14 @@
+package com.example.sportcaveapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Reaction extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reaction);
+    }
+}
