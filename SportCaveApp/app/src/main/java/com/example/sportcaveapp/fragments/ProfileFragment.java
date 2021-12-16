2cd39de83a9ef5c285e51f2894f1361710eeec4e
hajimariyam <hajimariyam1@gmail.com>
2021-12-15 19:38:25 -0600

    Take profile picture, or display default

--- SportCaveApp/app/src/main/java/com/example/sportcaveapp/fragments/ProfileFragment.java
+++ SportCaveApp/app/src/main/java/com/example/sportcaveapp/fragments/ProfileFragment.java
@@ -1,12 +1,23 @@
 package com.example.sportcaveapp.fragments;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -27,10 +38,15 @@ import com.parse.SaveCallback;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.io.File;
+
 public class ProfileFragment extends Fragment {
 
     ParseUser currentUser;
     ImageView ivProfile;
+    Button btnImage;
+    private File photoFile;
+    public String photoFileName = "photo.jpg";
     EditText etName;
     TextView tvUsername;
     EditText etEmail;
@@ -57,6 +73,7 @@ public class ProfileFragment extends Fragment {
 
         currentUser = ParseUser.getCurrentUser();
         ivProfile = view.findViewById(R.id.ivProfile);
+        btnImage = view.findViewById(R.id.btnImage);
         etName = view.findViewById(R.id.etName);
         tvUsername = view.findViewById(R.id.tvUsername);
         etEmail = view.findViewById(R.id.etEmail);
@@ -64,8 +81,17 @@ public class ProfileFragment extends Fragment {
         btnUpdate = view.findViewById(R.id.btnUpdate);
         btnLogout = view.findViewById(R.id.btnLogout);
 
+        btnImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchCamera();
+            }
+        });
 
-//        Glide.with(getContext()).load(currentUser.getParseFile("profilePicture").getUrl()).into(ivProfile);
+        if (currentUser.getParseFile("profilePicture") != null) {
+            Glide.with(getContext()).load(currentUser.getParseFile("profilePicture").getUrl()).into(ivProfile); }
+        else {
+            Glide.with(getContext()).load(R.drawable.profile).into(ivProfile); }
         etName.setText(currentUser.get("profileName").toString());
         tvUsername.setText("@"+currentUser.getUsername());
         etEmail.setText(currentUser.getEmail());
@@ -78,6 +104,9 @@ public class ProfileFragment extends Fragment {
                 String Email = etEmail.getText().toString();
                 String MySports = etMySports.getText().toString();
                 currentUser = ParseUser.getCurrentUser();
+                if (photoFile != null) {
+                    currentUser.put("profilePicture", new ParseFile(photoFile));
+                }
                 currentUser.put("profileName", Name);
                 currentUser.setEmail(Email);
                 currentUser.put("favSports", MySports);
@@ -105,10 +134,57 @@ public class ProfileFragment extends Fragment {
 
     }
 
+    ActivityResultLauncher<Intent> someActivityResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    // check if user took photo
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        // by this point we have the camera photo on disk
+                        Bitmap takenImage = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
+                        // Load the taken image into a preview
+                        ivProfile.setImageBitmap(takenImage);
+                    } else { // Result was a failure
+                        Toast.makeText(getContext(), "Picture wasn't taken!", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+
+
+    private void launchCamera() {
+        // create Intent to take a picture and return control to the calling application
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Create a File reference for future access
+        photoFile = getPhotoFileUri(photoFileName);
+
+        // wrap File object into a content provider
+        // Where to save taken image
+        Uri fileProvider = FileProvider.getUriForFile(getContext(), "com.codepath.fileprovider.sportcaveapp", photoFile);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
+
+        // Almost all phones have camera so will always be null
+        if (intent.resolveActivity(getContext().getPackageManager()) != null) {
+            // Start the image capture intent to take photo
+            someActivityResultLauncher.launch(intent);
+        }
+    }
+
+
+    // Returns the File for a photo stored on disk given the fileName
+    public File getPhotoFileUri(String fileName) {
+        File mediaStorageDir = new File(getContext().getExternalFilesDir(Environment.DIRECTORY_PICTURES), TAG);
+
+        // Create the storage directory if it does not exist
+        if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()){
+            Log.d(TAG, "failed to create directory"); }
+
+        return new File(mediaStorageDir.getPath() + File.separator + fileName);
+    }
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
     }
 
 }
